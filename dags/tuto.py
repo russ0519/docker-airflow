ec55a552ff4ad2ec05b0ecaa3f1add1fb348b60a
Ruslan Sivak <git@ruslansivak.com>
2019-03-10 23:40:13 -0400

    change catchup to False

--- dags/tuto.py
+++ dags/tuto.py
@@ -16,6 +16,7 @@ default_args = {
     "email_on_retry": False,
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
+    "catchup": False
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
