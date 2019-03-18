1811551b00b6c59cf33aff6d0c8224dea1a1274c
Ruslan Sivak <git@ruslansivak.com>
2019-03-18 19:12:51 -0400

    removing backfill

--- dags/tuto.py
+++ dags/tuto.py
@@ -15,15 +15,14 @@ default_args = {
     "email_on_failure": False,
     "email_on_retry": False,
     "retries": 1,
-    "retry_delay": timedelta(minutes=5),
-    "catchup": False
+    "retry_delay": timedelta(minutes=5)
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
 }
 
-dag = DAG("tutorial", default_args=default_args, schedule_interval=timedelta(1))
+dag = DAG("tutorial", default_args=default_args, catchup=False, schedule_interval=timedelta(1))
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
 t1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
